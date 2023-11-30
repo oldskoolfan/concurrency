17db9f879a3585ad6dce033d998c3a04b0f04f47
Andrew Harris <aharri10@cscc.edu>
2023-11-29 20:20:14 -0500

    multiple threads

--- src/channels.rs
+++ src/channels.rs
@@ -1,13 +1,39 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread, time::Duration};
 
 pub fn run_channels() {
   let (tx, rx) = mpsc::channel();
+  let tx_clone = tx.clone();
 
   thread::spawn(move || {
-    let val = String::from("hi");
-    tx.send(val).unwrap();
+    let vals = vec![
+      String::from("hi"),
+      String::from("from"),
+      String::from("the"),
+      String::from("thread"),
+    ];
+
+    for val in vals {
+      tx.send(val).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
   });
 
-  let received = rx.recv().unwrap();
-  println!("Got: {}", received);
+  thread::spawn(move || {
+    let vals = vec![
+      String::from("more"),
+      String::from("messages"),
+      String::from("for"),
+      String::from("you"),
+    ];
+
+    for val in vals {
+      tx_clone.send(val).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
+  });
+
+  for received in rx {
+    println!("Got: {}", received);
+  }
+  println!("Done with main");
 }
